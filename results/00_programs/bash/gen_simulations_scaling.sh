ccc3f432aa17702ed05ee78ade78c96490298c85
Matthew Dorsey <madorse2@ncsu.edu>
2024-06-05 14:20:19 +0200

    Added method for generating parmaeterized simulation executables.

--- results/00_programs/bash/gen_simulations_scaling.sh
+++ results/00_programs/bash/gen_simulations_scaling.sh
@@ -20,9 +20,9 @@ declare -i BOOL_UPP=0
 declare -i BOOL_DWN=0
 ## PARAMETERS -- JOB
 # array containing N to test
-declare -A PARM_N=( 32 48 64 78 96 128 256 )
+PARM_N=( 32 48 64 78 96 128 256 )
 # array containing the different executables to test
-declare -A PARM_EX=( "linearChainReal" "linearChainIdeal" )
+PARM_EX=( "linearChainReal" "linearChainIdeal" )
 # number of times to replicate each unique set of simualation conditions
 declare -i PARM_R=3
 # default directory for upload / download, generating parameters
@@ -31,6 +31,15 @@ MAINDIR="01_raw_data"
 JOB="scaling"
 # linux server
 LINUXSERV="gandalf"
+# path to LeMonADE executables
+EXECDIR="00_programs/build/bin/"
+## PARAMETERS -- SIMULATION
+# number of MCSs between each property calculation
+declare -i N_MCS=200000
+# number of time properties are calculation
+declare -i N_runs=50000
+# number of MCSs before equilibrium properties are calculated
+declare -i t_equilibrium=1000000
 
 
 ## FUNCTIONS
@@ -40,7 +49,7 @@ help () {
 	# TODO :: write options so that if a variable is specified, its value is held constant
 	# if the job already exists, the script skips, unless and overwrite flag is specified
 
-	echo -e "\nScript for generating conH jobs on CHTC systems.\nUSAGE: ./gen_scaling simulations.sh << FLAGS >>\n"
+	echo -e "\nScript for generating polymer simulations with BFM model on linux clusters.\nUSAGE: ./gen_scaling simulations.sh << FLAGS >>\n"
 	echo -e "\n"
 	echo -e " ## SCRIPT PROTOCOL ##"
 	echo -e " -h           | display script options, exit 0."
@@ -54,6 +63,48 @@ help () {
 	echo -e " -j << ARG >> | specify job title (default is ${JOB})."
 	echo -e " -l << ARG >> | specify linux cluster (default is ${LINUXSERV})."
 	echo -e " -p << ARG >> | specify the local directory (default is ${MAINDIR})."
+	echo -e " -e << ARG >> | specify path to LeMonADE executables (default is ${EXECDIR})."
+	echo -e "\n"
+}
+
+# generate simulation parameters, write to file
+gen_simparm() {
+
+	## PARAMETERS
+	# path to simulation directories
+	PATH_SIMPARM="${MAINDIR}/${JOB}/"
+	# file to write simulation parameters to
+	FILE_SIMPARM="${PATH_SIMPARM}${JOB}.csv"
+	# header used for the simulation parameter file
+	HEADER_SIMPARM="id,mod,N,R,path"
+
+	## ARGUMENTS
+	# none
+
+	## SCRIPT
+	# if the path does not exist, make the directories
+	if [ ! -d $PATH_SIMPARM ]; then
+		mkdir $PATH_SIMPARM
+	fi
+
+	# write head to file, write parameters to file
+	echo $HEADER_SIMPARM > $FILE_SIMPARM
+	for m in "${PARM_EX[@]}"; do
+		for n in "${PARM_N[@]}"; do
+			for r in $(seq 1 $PARM_R); do
+				# generate the simulation directory
+				SIMID="${m}_N${n}R${r}"
+				SIMDIR="${m}/N${n}/R${r}/"
+				mkdir -p ${PATH_SIMPARM}${SIMDIR}
+				# write files directory, generate simulation executables
+				cp $EXECDIR${m} ${PATH_SIMPARM}${SIMDIR}
+				echo "./$m ${n} ${N_MCS} ${N_runs} ${t_equilibrium}" > ${PATH_SIMPARM}${SIMDIR}${SIMID}.sh
+				chmod u+x ${PATH_SIMPARM}${SIMDIR}${SIMID}.sh
+				# add parameters to parm file
+				echo "${SIMID},${m},${n},${r},${SIMDIR}" >> $FILE_SIMPARM
+			done
+		done
+	done
 }
 
 
@@ -92,4 +143,8 @@ done
 
 ## SCRIPT
 # generate parameters save to file
+if [ $BOOL_GEN -eq 1 ]; then
+	gen_simparm
+fi
 
+# parse simulation parameters from file
