ee52c38a0d707ae1d9edeead766e12f3f3a31908
Matthew Dorsey <madorse2@ncsu.edu>
2024-06-11 15:43:16 +0200

    Added initial script for generating force extension simulations.

---
+++ results/00_programs/bash/gen_simulations_forceExtension.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+## Matthew Dorsey
+## 2024.06.11
+##  script for generating constant force extension simulations for real and ideal polymer chains
+
+## PARAMETERS
+# none
+
+
+## OPTIONS
+# none
+
+
+## FUNCTIONS
+# none
+
+
+## ARGUMENTS
+# none
+
+
+## SCRIPT
+# none
