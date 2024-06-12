9708a977c23a82e76d3d2059a27111cc3928dbec
Matthew Dorsey <madorse2@ncsu.edu>
2024-06-12 10:49:40 +0200

    Added methods for generating simulation parameters.

--- results/00_programs/bash/gen_simulations_forceExtension.sh
+++ results/00_programs/bash/gen_simulations_forceExtension.sh
@@ -29,7 +29,7 @@ MIN_FORCE_VAL=".0001"
 # array containing N to test
 PARM_N=( 100 )
 # array containing the different executables to test
-PARM_EX=( "FElinearChainReal" "FElinearChainIdeal" )
+PARM_EX=( "FElinearChainReal" ) # "FElinearChainIdeal"
 # number of times to replicate each unique set of simualation conditions
 declare -i PARM_R=1
 # default directory for upload / download, generating parameters
@@ -70,9 +70,101 @@ help () {
 	echo -e "\n"
 }
 
+# log10 function, echos log10 of first argument passed to method
+log10 (){
+
+    ## PARAMETERS
+    # none
+
+    ## ARGUMENTS
+    # number to perform log10 operation on
+    local NUM_LOG=$1
+
+    ## SCRIPT
+    # perform log10 operation on number
+    echo "l(${NUM_LOG})/l(10)" | bc -l
+}
+
+# pow10 function, echos 10 to the power of the first argument passed to method
+pow10 () {
+
+    ## PARAMETERS
+    # none
+
+    ## ARGUMENTS
+    # number to perform pow10 operation on
+    local NUM_POW=$1
+
+    ## SCRIPT
+    # perform pow10 operation
+    VAL=$( echo "l(10)" | bc -l )
+    VAL=$( echo "(${VAL})*(${NUM_POW})" | bc -l )
+    echo "e(${VAL})" | bc -l
+}
+
+# used to generate parameters along a logscale
+logscale () {
+
+     ## PARAMETERS
+     # minimum number on a log10 scale
+     MIN_VAL_LOG10=$( log10 ${MIN_FORCE_VAL} )
+     # maximum number on a log10 scale
+     MAX_VAL_LOG10=$( log10 ${MAX_FORCE_VAL} )
+
+     ## ARGUMENTS
+     # integer, ranging from 1 to N_FORCE_VAL
+     declare -i NUM=$1
+
+     ## SCRIPT
+     # generate the parameter along scale
+     scale=$( echo "($NUM / ( ${N_FORCE_VAL} - 1 ))" | bc -l )
+     scale=$( echo "(${scale} * (${MAX_VAL_LOG10} - ${MIN_VAL_LOG10}) + ${MIN_VAL_LOG10})" | bc -l )
+     scale=$( pow10 "$scale" )
+     echo $(printf "%8.5f\n" "${scale}")
+}
+
 # generate simulation parameters
-generate_simulation_parameters() {
-    ## TODO implement simulations parameters
+gen_simparm() {
+
+	## PARAMETERS
+	# path to simulation directories
+	PATH_SIMPARM="${MAINDIR}/${JOB}/"
+	# file to write simulation parameters to
+	FILE_SIMPARM="${PATH_SIMPARM}${JOB}.csv"
+	# header used for the simulation parameter file
+	HEADER_SIMPARM="id,mod,N,F,R,path"
+
+	## ARGUMENTS
+	# none
+
+	## SCRIPT
+	# if the path does not exist, make the directories
+	if [ ! -d $PATH_SIMPARM ]; then
+		mkdir $PATH_SIMPARM
+	fi
+
+	# write head to file, write parameters to file
+	echo $HEADER_SIMPARM > $FILE_SIMPARM
+	for m in "${PARM_EX[@]}"; do
+		for n in "${PARM_N[@]}"; do
+            for f in $(seq 0 $(($N_FORCE_VAL-1))); do
+                for r in $(seq 1 $PARM_R); do
+                    # generate the simulation directory
+                    SIMID="${m}_N${n}F${f}R${r}"
+                    SIMDIR="${m}/N${n}/F${f}/R${r}/"
+                    mkdir -p ${PATH_SIMPARM}${SIMDIR}
+                    # use the force integer to calculate the real force value passed to the simulation executable
+                    FORCE_VAL=$( logscale $f )
+                    # write files directory, generate simulation executables
+                    cp $EXECDIR${m} ${PATH_SIMPARM}${SIMDIR}
+                    echo "./$m ${n} ${N_MCS} ${N_runs} ${t_equilibrium} ${FORCE_VAL} > ${SIMID}.txt" > ${PATH_SIMPARM}${SIMDIR}${SIMID}.sh
+                    chmod u+x ${PATH_SIMPARM}${SIMDIR}${SIMID}.sh
+                    # add parameters to parm file
+                    echo "${SIMID},${m},${n},${r},${FORCE_VAL},${SIMDIR}" >> $FILE_SIMPARM
+                done
+            done
+		done
+	done
 }
 
 ## OPTIONS
