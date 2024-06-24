dad2dbfcf5e512e3827dd4ba17cc98553c7b4381
Matthew Dorsey <madorse2@ncsu.edu>
2024-06-24 11:07:01 +0200

    Added pseudocode for linux program that submits simulations to SLURM linux clutser.

---
+++ results/00_programs/bash/submit_batch_job.sh
@@ -0,0 +1,26 @@
+## Matthew A. Dorsey
+## Intitute for Polymer Resarch - Institute for Polymer Theory
+## North Carolina State University
+## 2024.06.24
+
+## script that submits a set of parameterized jobs for execution.
+## the script on accepts a file that submits the simulations for execution.
+## the script can either run on a local cluster, or submit on HPC cluster at IPF / ITP.
+
+## PARAMETERS
+# none
+
+## FUNCTIONS
+# none
+
+## OPTIONS
+# none
+
+## ARGUMENTS
+# none
+
+## SCRIPT
+# parse csv file, get simulation id and directory
+# submit simulations
+# either loop through locally and submit serial
+# or logon to computing cluster and exectue
