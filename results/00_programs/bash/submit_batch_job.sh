0651f26a3b3211aa06865268bd8d9013adb82458
Matthew Dorsey <madorse2@ncsu.edu>
2024-06-24 11:25:46 +0200

    Added options and help.

--- results/00_programs/bash/submit_batch_job.sh
+++ results/00_programs/bash/submit_batch_job.sh
@@ -8,18 +8,96 @@
 ## the script can either run on a local cluster, or submit on HPC cluster at IPF / ITP.
 
 ## PARAMETERS
-# none
+# boolean that determines if the script should execute verbosely
+declare -i BOOL_VERB=0
+# boolean that determines if a parameterized file has been passed to the method
+declare -i BOOL_FILE=0
+# boolean that determines if a path for the simulation directory has been passed to the method
+declare -i BOOL_PATH=0
+# boolean that determines if a job title has been passed to the method
+declare -i BOOL_JOB=0
+# boolean that determines if the job should be submitted on a remote linux cluster
+declare -i BOOL_SUBMIT_SLURM=0
+# default linux server used for remote jobs
+LINUXSERV="gandalf"
+# default path to parameterized linux executables for job
+EXECDIR="00_programs/build/bin/"
 
 ## FUNCTIONS
-# none
+# report script used to command line terminal
+help() {
+
+    ## PARAMETERS
+    # none
+
+    ## ARGUMENTS
+    # none
+
+    ## SCRIPT
+    # report usage
+	echo -e "\nScript for executing parameterized jobs on linux clusters.\nUSAGE: ./submit_batch_job.sh << FLAGS >>\n"
+	echo -e "\n"
+	echo -e " ## SCRIPT PROTOCOL ##"
+	echo -e " -h           | display script options, exit 0."
+	echo -e " -v           | execute script verbosely."
+	echo -e " -s           | submit job to SLURM via remote linux cluster (otherwise run locally in serial)."
+# 	echo -e " -u           | upload local default directory to linux cluster."
+# 	echo -e " -d           | sync local default directory with linux cluster."
+	echo -e "\n"
+	echo -e " ## SCRIPT PARAMETERS ##"
+	echo -e " -j << ARG >> | MANDATORY: specify job title."
+	echo -e " -p << ARG >> | specify path to simulation job directory hirearchy (default is 01_raw_data/\${JOB})."
+	echo -e " -f << ARG >> | file which contains simulation parameters (default is 01_raw_data/\${JOB}/\${JOB}.csv)"
+	echo -e " -l << ARG >> | specify remote linux cluster (default is ${LINUXSERV})."
+	echo -e " -e << ARG >> | specify path to LeMonADE executables (default is ${EXECDIR})."
+	echo -e "\n"
+}
+
+# checks that correct options have been passed to method before script execution
+check () {
+
+    ## PARAMETERS
+    # none
+
+    ## ARGUMENTS
+    # none
+
+    ## SCRIPT
+    # check option booolean
+}
 
 ## OPTIONS
-# none
+while getopts "hvsj:p:f:l:e:" option; do
+	case $option in
+		h) # print script parameters to CLT
+			help
+			exit 0 ;;
+        v) # exectue script verbosely
+            declare -i BOOL_VERB=1 ;;
+		s) # submit simulations to SLURM
+			declare -i BOOL_SUBMIT_SLURM=1 ;;
+		j) # update job name
+			JOB="${OPTARG}" ;;
+        p) # update path
+            PATH="${OPTARG}";;
+        f) # update parameter file
+            FILE="${OPTARG}";;
+		l) # update linux cluster
+			LINUXSERV="${OPTARG}" ;;
+        e) # update LeMonADE executables
+            EXECDIR="${OPTARG}";;
+        \?) # sonstiges
+            help
+            exit $NONZERO_EXITCODE
+    esac
+done
 
 ## ARGUMENTS
 # none
 
 ## SCRIPT
+# check that correct arguments have been passed to the method
+check
 # parse csv file, get simulation id and directory
 # submit simulations
 # either loop through locally and submit serial
