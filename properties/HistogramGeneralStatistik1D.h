16a5c70092fcff6fe8320706c8180a5ae14edf54
Matthew Dorsey <madorse2@ncsu.edu>
2024-10-17 14:17:30 +0200

    Added comments to histogram methods, fixed bug in variance calculation.

--- properties/HistogramGeneralStatistik1D.h
+++ properties/HistogramGeneralStatistik1D.h
@@ -14,6 +14,7 @@ class HistogramGeneralStatistik1D
 {
 public:
 
+	// initializer for 1d histogram
 	HistogramGeneralStatistik1D()
 	{
 		nBins = 0;
@@ -22,6 +23,8 @@ public:
 		maxVal = 0.0;
 		binsize = 0.0;
 	}
+
+	// contructor for 1d histogram
 	HistogramGeneralStatistik1D(double min, double max, int nbins)
 	{
 		
@@ -45,6 +48,7 @@ public:
 	{
 	}
 	
+	// resets entire histogram with new minimum, maximum and bin sizes
 	void reset(double min=0.0,double max=0.0,size_t nbins=0)
 	{
 		histogram.clear();
@@ -66,6 +70,7 @@ public:
 		histogram_count = {0};
 	}
 	
+	// geth the bin number (integer) associated with a value
 	size_t getBinNo(double x) const
 	{
 		if(x<minVal)
@@ -76,12 +81,13 @@ public:
 		return uint32_t( std::floor((x-minVal)/binsize));
 	}
 	
+	// returns the center of a histogram bin that is closest to the value the is passed to the method
 	double getClosestBinCenter(double x) const
 	{
 		return (minVal+getBinNo(x)*binsize + binsize/2.0);
 	}
 	
-	
+	// returns the normalized average associated with bin integer
 	double getFirstMomentInBin(size_t bin) const
 	{
 		return histogram[bin] / nValues;
@@ -92,32 +98,38 @@ public:
 	// 	return histogram[bin].ReturnM2();
 	// }
 
-	double getNumCountAt(double x) const
+	// returns the number of counts associated with a bin
+	double getNumCountAtKey(double x) const
 	{
 		return histogram_count[getBinNo(x)];
 	}
-	
+
+	// returns the normalized average associated with double
 	double getFirstMomentAtKey(double x) const
 	{
 		return histogram[getBinNo(x)] / nValues;
 	}
 
-	double getCountAt(double x) const
+	// gets the unnormalized count associated with double
+	double getCountAtKey(double x) const
 	{
 		return histogram[getBinNo(x)];
 	}
 	
+	// returns the value associated with the center of a bin
 	double getCenterOfBin(int bin) const
 	{
 		return (minVal+(bin)*binsize + binsize/2.0);
 	}
 	
-	double getLowerBoundaryOfBin(int bin) const
+	// returns the value associated with the lower boundary of a bin
+	double getBinLowerBoundary(int bin) const
 	{
 		return (minVal+(bin)*binsize);
 	}
 
-	double getHigherBoundaryOfBin(int bin) const
+	// returns the value associated with the upper boundary of a bin
+	double getBinUpperBoundary(int bin) const
 	{
 		return (minVal+(bin+1)*binsize);
 	}
@@ -126,9 +138,11 @@ public:
 	double getHistAverage () const
 	{
 		double avg = 0;
+		double sum = 0;
 		// loop through each bin, add the statistical contribution of the bins value to the sum
 		for (int n = 0; n < nBins; n++) {
-			avg += histogram[n] * (minVal+(n)*binsize + binsize/2.0);
+			avg += histogram[n] * getCenterOfBin(n);
+			sum += histogram_count[n];
 		}
 		return avg / nValues;
 	}
@@ -137,38 +151,46 @@ public:
 	double getHistVariance() const
 	{
 		double avg = getHistAverage();
+		double sum = 0;
 		double var = 0.;
 		// loop through, take the square difference between the average and the bin value
 		for (int n = 0; n < nBins; n++) {
-			var += std::pow(((histogram[n] * (minVal + ((n) * binsize) + (binsize / 2.))) - avg), 2.);
+			var += histogram[n] * std::pow((getCenterOfBin(n) - avg), 2.);
+			sum += histogram_count[n];
 		}
-		return var / (nValues - 1);
+		return var / (sum - 1);
 	}
 
 	// return standard deviation of histogram
 	double getHistSTD () const {return std::sqrt(getHistVariance());}
 
+	// returns the number of bins in histogram
 	int getNBins() const
 	{
 		return nBins;
 	}
 
+	// returns the total number of times values have been accumulated within the histogram
 	double getNCounts() const
 	{
 		return nValues;
 	}
 	
-	
+	// returns the width of each histogram bin
 	double getBinwidth() const
 	{
 		return binsize;
 	}
 	
+	// returns the maximum value which can be accepted by the histogram (upper boundary of last bin)
 	double getMinCoordinate() const{return minVal;}
+
+	// returns the minimum value which can be accepted by the histogram (lower boundary of first bin)
 	double getMaxCoordinate() const{return maxVal;}
 
 	//double getValueSum() const{return valueSum;}
 
+	// increments a histogram bin associated with double (x) by a statistical weight (usually one)
 	void addValue(double x, double statisticalWeight=1.0) 
 	{
 		
@@ -185,6 +207,7 @@ public:
 		nValues+=1.0;
 	}
 	
+	// resets a histogram bin associated with a value (x), including the bin count, to a statistical weight
 	void resetValue(double x, double statisticalWeight=1.0)
 	{
 
@@ -204,6 +227,7 @@ public:
 		nValues+=1.0;
 	}
 
+	// resets the total number of histogram counts to the count associated with each bin
 	void reset_values_count ()
 	{
 		nValues = 0.;
@@ -211,14 +235,8 @@ public:
 			nValues += (int) histogram_count[nBins];
 		}
 	}
-	
-	// const std::vector<StatisticMoment>& getVectorValues() const
-	// {
-	// 	return histogram;
-	// }
 
-
-
+	// returns the center of each bin in the histogram as an array
 	std::vector<double> getVectorBins() const
 	{
 		std::vector<double> positions;
