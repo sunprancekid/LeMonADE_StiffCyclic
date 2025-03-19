764a36d925f0717d5b48c93c573d8f2c47714421
Matthew Dorsey <madorse2@ncsu.edu>
2025-03-19 16:40:00 +0100

    Bug fix. Was reading in string for filename as interger.

--- projects/simulatePolymerBFM/simulateIdealPolymerBFM.cpp
+++ projects/simulatePolymerBFM/simulateIdealPolymerBFM.cpp
@@ -82,7 +82,7 @@ int main(int argc, char* argv[])
                     infile = optarg;
                     break;
                 case 'o':
-                    outfile = atoi(optarg);
+                    outfile = optarg;
                     break;
                 case 's':
                     save_interval=atoi(optarg);
