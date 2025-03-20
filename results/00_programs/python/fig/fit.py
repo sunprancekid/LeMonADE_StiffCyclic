9e020677842afeb14e6aaf45a92300694a007cee
Matthew Dorsey <madorse2@ncsu.edu>
2025-03-20 16:04:40 +0100

    Initial commit for line class.

---
+++ results/00_programs/python/fig/fit.py
@@ -0,0 +1,41 @@
+## Matthew A. Dorsey
+## @sunprancekid
+## contains Line class, and methods to fit data to certain relationships
+
+##############
+## PACKAGES ##
+##############
+
+import pandas as pd
+import os # used to check path
+import itertools # used for iterating over markers
+
+
+################
+## PARAMETERS ##
+################
+# none
+
+
+#############
+## METHODS ##
+#############
+# none
+
+
+#############
+## CLASSES ##
+#############
+# none
+
+
+###############
+## ARGUMENTS ##
+###############
+# none
+
+
+############
+## SCRIPT ##
+############
+# none
